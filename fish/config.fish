16f07a72bb01d06111f7f7a2e1cdee3e867e92f5
Paweł Renc <rencpawe@gmail.com>
2021-11-26 20:35:59 +0100

    Resolve conflitcts after adding def browser env

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 fish_vi_key_bindings
+set fish_greeting
 
 alias v='vim'
 
@@ -14,10 +15,10 @@ alias llt='ll -t'
 alias dm='cd ~/Documents'
 alias dw='cd ~/Downloads'
 
-set -x EDITOR vim
-set -x BROWSER firefox
-set -x PAGER less
-set -x TERM xterm-256color
+set -gx EDITOR vim
+set -gx BROWSER firefox
+set -gx PAGER less
+set -gx TERM xterm-256color
 
 function fish_user_key_bindings
     for mode in insert default visual
